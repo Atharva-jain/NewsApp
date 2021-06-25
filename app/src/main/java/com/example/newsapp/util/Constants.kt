d5a784884736573ea4fa3ba4f8affb58631e3ac8
Mainsha jain <Atharvajain01@gmail.com>
2021-06-25 17:20:21 +0530

    Api key update

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "5433daf983c34ef5ad5851112d9fad01"
+        const val API_KEY = "Your Api Key"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
